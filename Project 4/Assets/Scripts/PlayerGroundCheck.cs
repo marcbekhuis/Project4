12ad464a10414652e0a73ca069a87aee9e1722d4
marc <marc@bekhuis.net>
2019-05-06 12:15:52 +0200

    PlayerMovement

---
+++ Project 4/Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerMovement.inAir = false;
+        Debug.Log("Check");
+    }
+}
---
+++ Project 4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public float speed = 300;
+    public float jumpForce = 1000;
+    public bool inAir = false;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed,rigidbody.velocity.y);
+        if (!inAir)
+        {
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                rigidbody.AddForce(new Vector2(0, jumpForce * Time.fixedDeltaTime));
+                inAir = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inAir = false;
+        Debug.Log("check");
+    }
+}
