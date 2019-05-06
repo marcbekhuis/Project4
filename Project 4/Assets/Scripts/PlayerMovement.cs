e87f68445c6f26749bd365abff9525e797c455af
marc <marc@bekhuis.net>
2019-05-06 14:27:38 +0200

    Stuff

--- Project 4/Assets/Scripts/PlayerMovement.cs
+++ Project 4/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigidbody;
-    public float speed = 300;
+    public float walkSpeed = 250;
+    public float runSpeed = 500;
+    public float crouchSpeed = 100;
     public float jumpForce = 1000;
     public bool inAir = false;
 
@@ -16,12 +18,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed,rigidbody.velocity.y);
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.fixedDeltaTime * runSpeed, rigidbody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.fixedDeltaTime * crouchSpeed, rigidbody.velocity.y);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.fixedDeltaTime * walkSpeed, rigidbody.velocity.y);
+        }
         if (!inAir)
         {
             if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
-                rigidbody.AddForce(new Vector2(0, jumpForce * Time.fixedDeltaTime));
+                rigidbody.AddForce(new Vector2(0, jumpForce * Time.fixedDeltaTime), ForceMode2D.Impulse);
                 inAir = true;
             }
         }
