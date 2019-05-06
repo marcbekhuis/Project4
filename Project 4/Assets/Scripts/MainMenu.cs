f72b095c031ed3ef59e8453ac0eeeed34c3363f1
Isoply <50321050+Isoply@users.noreply.github.com>
2019-05-06 12:57:13 +0200

    MainMenu

---
+++ Project 4/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private int nextScene;
+
+    public void StartGamer()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
