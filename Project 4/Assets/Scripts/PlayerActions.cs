85d14fbc9140445bba12d834ce89f345b4d93f68
marc <marc@bekhuis.net>
2019-05-07 13:03:30 +0200

    Tree

--- Project 4/Assets/Scripts/PlayerActions.cs
+++ Project 4/Assets/Scripts/PlayerActions.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerActions : MonoBehaviour
 {
     public GameObject infoBook;
+    public Tilemap tilemap;
 
     // Update is called once per frame
     void Update()
@@ -14,11 +16,19 @@ public class PlayerActions : MonoBehaviour
             if (infoBook.activeSelf)
             {
                 infoBook.SetActive(false);
+                Time.timeScale = 1;
             }
             else
             {
                 infoBook.SetActive(true);
+                Time.timeScale = 0;
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            tilemap.SetTile(new Vector3Int((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0), null);
+            Debug.Log(tilemap.HasTile(new Vector3Int((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0)));
+            Debug.Log(Input.mousePosition);
         }
     }
 }
---
+++ Project 4/Assets/Scripts/SkyBoxMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, 0);
+    }
+}
