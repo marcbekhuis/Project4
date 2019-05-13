c337701d1cf49230716d6688b6997975ca0559ad
Myrina <jinkisaragi@hotmail.com>
2019-05-13 11:19:02 +0200

    Lava Update haHAA

---
+++ Project 4/Assets/Scripts/Lava.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    [SerializeField] private Transform respawnPoint;
+
+  
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject hitObj = collider.gameObject;
+
+        if (hitObj.tag == "Player")
+        {
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+
+}
