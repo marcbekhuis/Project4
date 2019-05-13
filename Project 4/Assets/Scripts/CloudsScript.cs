34d13f2a70d540413fe9526644aec0c663a32748
Isoply <50321050+Isoply@users.noreply.github.com>
2019-05-13 13:06:24 +0200

    Added clouds
    -Added IsoplysTestWorld
    -Added clouds in IsoplysTestWorld

---
+++ Project 4/Assets/Scripts/CloudsScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsScript : MonoBehaviour
+{
+    float scrollSpeed;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        scrollSpeed = Random.Range(-1, -4);
+    }
+    private void Update()
+    {
+        rb.velocity = new Vector2(scrollSpeed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("EndWall"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
