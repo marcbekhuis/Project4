887026ce11471537b98bd84dc04afd9547f9cc3c
Isoply <50321050+Isoply@users.noreply.github.com>
2019-05-14 09:13:50 +0200

    Fixed cloud collision

---
+++ Project 4/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    float endVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endVolume = audioSource.volume;
+        audioSource.volume = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioSource.volume != endVolume)
+        {
+            audioSource.volume += 1 * Time.deltaTime;
+        }
+    }
+}
