f85d87d8f9d779cd1af2912380d07f90b9ad517b
Isoply <50321050+Isoply@users.noreply.github.com>
2019-05-15 09:34:08 +0200

    p

---
+++ Project 4/Assets/Scripts/DeathSceneCloudScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSceneCloudScroller : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    [SerializeField] int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.velocity = new Vector3(-speed, 0, 0);
+        if (transform.position.x <= -13)
+        {
+            transform.position = new Vector3(13, 0, 0);
+        }
+    }
+}
