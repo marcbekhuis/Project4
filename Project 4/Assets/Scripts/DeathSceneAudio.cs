e31c4caddc3e39a99cc966d28b9145725b6c262c
Isoply <50321050+Isoply@users.noreply.github.com>
2019-05-15 10:16:35 +0200

    Added new backgroundmusic

---
+++ Project 4/Assets/Scripts/DeathSceneAudio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSceneAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip deathNoise;
+    public AudioClip deathMusic;
+
+    float endVolume;
+    bool playedOnce;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = deathNoise;
+        audioSource.volume = 0.25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playedOnce == true)
+        {
+            if (audioSource.volume < endVolume)
+            {
+                audioSource.volume += 0.02f * Time.deltaTime;
+            }
+        }
+
+        timer += 1 * Time.deltaTime;
+        if (timer >= 6)
+        {
+            playedOnce = true;
+
+            endVolume = audioSource.volume;
+            audioSource.volume = 0;
+
+            audioSource.clip = deathMusic;
+        }
+    }
+}
