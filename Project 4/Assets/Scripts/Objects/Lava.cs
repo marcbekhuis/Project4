73d5de6dc52bfeef624b33516d7209bfef6d9401
Myrina <jinkisaragi@hotmail.com>
2019-05-23 13:00:55 +0200

    MORE COMMENTS

--- Project 4/Assets/Scripts/Objects/Lava.cs
+++ Project 4/Assets/Scripts/Objects/Lava.cs
@@ -8,13 +8,15 @@ public class Lava : MonoBehaviour
 
     [SerializeField] private Transform respawnPoint;
 
-  
+    //Calls function when the trigger is entered
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        
         GameObject hitObj = collider.gameObject;
-
+        //Detects if the entered object has the tag "Player"
         if (hitObj.tag == "Player")
         {
+            //Sends the plater to the transform of the respawn point
             player.transform.position = respawnPoint.transform.position;
         }
     }
