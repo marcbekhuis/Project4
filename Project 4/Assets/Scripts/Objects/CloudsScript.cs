816d80ea85e0eb795078964de6ea58773a0a7cf1
Myrina <jinkisaragi@hotmail.com>
2019-05-23 11:31:58 +0200

    paar scripts

--- Project 4/Assets/Scripts/Objects/CloudsScript.cs
+++ Project 4/Assets/Scripts/Objects/CloudsScript.cs
@@ -9,15 +9,18 @@ public class CloudsScript : MonoBehaviour
 
     private void Start()
     {
+        //Sets a random number within a certain range for the variable scrollSpeed
         scrollSpeed = Random.Range(-1, -4);
     }
     private void Update()
     {
+        //Sets velocity to the rigidbody of this object
         rb.velocity = new Vector2(scrollSpeed, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Checks collision with the game object tagged as "EndWall" and destroys the object with this script attached
         if (collision.gameObject.CompareTag("EndWall"))
         {
             Destroy(this.gameObject);
