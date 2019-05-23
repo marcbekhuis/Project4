793292a58c97d7b8a661b77f97ae698a51177362
marc <marc@bekhuis.net>
2019-05-23 12:42:44 +0200

    Comments

--- Project 4/Assets/Scripts/Player/PlayerActions.cs
+++ Project 4/Assets/Scripts/Player/PlayerActions.cs
@@ -31,18 +31,22 @@ public class PlayerActions : MonoBehaviour
     private void Start()
     {
         cursor.transform.position = new Vector3(0, 0, 0);
+        // gets PlayerInventory from the object this script is on.
         playerInvetory = GetComponent<PlayerInvetory>();
         controllerMode = PlayerPrefs.GetInt("controllerMode");
     }
 
     private void FixedUpdate()
     {
+        // checks what controllerMode is set to.
         if (controllerMode == 0)
         {
+            // sets the position of the cursor gameobject.
             cursor.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x + offsetX, Camera.main.ScreenToWorldPoint(Input.mousePosition).y + offsetY, 0);
         }
         else
         {
+            // checks for inputs to move the cursor object
             if (Input.GetAxis("HorizontalController") > 0.25)
             {
                 xAxis++;
@@ -70,28 +74,35 @@ public class PlayerActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // checks if you pressed I
         if (Input.GetKeyDown(KeyCode.I))
         {
+            // checks if gameobject is active
             if (infoBook.activeSelf)
             {
+                // sets gameobject active to false
                 infoBook.SetActive(false);
                 allowAction = true;
             }
             else
             {
+                // sets gameobject active to true
                 infoBook.SetActive(true);
                 allowAction = false;
             }
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
+            // checks if gameobject is active
             if (inventory.activeSelf)
             {
+                // sets gameobject active to false
                 inventory.SetActive(false);
                 allowAction = true;
             }
             else
             {
+                // sets gameobject active to true
                 inventory.SetActive(true);
                 if (playerInvetory.selectedCraftingIcon != null)
                 {
@@ -102,34 +113,45 @@ public class PlayerActions : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P) || Input.GetKey(KeyCode.JoystickButton6))
         {
+            // checks if gameobject is active
             if (pauseMenu.activeSelf)
             {
+                // sets gameobject active to false
                 pauseMenu.SetActive(false);
                 allowAction = true;
                 gamePaused = false;
             }
             else
             {
+                // sets gameobject active to true
                 pauseMenu.SetActive(true);
                 allowAction = false;
                 gamePaused = true;
             }
         }
+        // checks if delay for action is passed
         if (actiondelay < Time.time)
         {
+            // checks if axe or notting is equiped
             if (axeEquipped || !axeEquipped && !pickaxeEquipped && !swordEquipped)
             {
+                // checks for input left mouse click
                 if (Input.GetKeyDown(KeyCode.Mouse0) && allowAction || Input.GetKeyDown(KeyCode.JoystickButton3) && allowAction)
                 {
+                    // checks if your overlapping a tree
                     if (overlappingTree != null)
                     {
+                        // gets tree info from the overlapping tree.
                         TreeInfo treeInfo = overlappingTree.GetComponent<TreeInfo>();
+                        // loops through the items you get from the tree and adds them to your inventory.
                         for (int x = 0; x < treeInfo.itemsAmount.Length; x++)
                         {
                             playerInvetory.Additem(treeInfo.itemsString[x], treeInfo.itemsAmount[x] / treeInfo.hits);
                         }
+                        // removes a hit from the tree.
                         treeInfo.currenthits--;
                         actiondelay = Time.time + 1;
+                        // destroys the tree.
                         if (treeInfo.currenthits <= 0)
                         {
                             Destroy(overlappingTree);
@@ -137,25 +159,35 @@ public class PlayerActions : MonoBehaviour
                     }
                 }
 
+                // check if you dont have anything equipped
                 if (!axeEquipped && !pickaxeEquipped && !swordEquipped)
                 {
-
+                    // checks for input right mouse click
                     if (Input.GetKeyDown(KeyCode.Mouse1) && allowAction || Input.GetKeyDown(KeyCode.JoystickButton3) && allowAction)
                     {
+                        // check if you have a item selected to build with.
                         if (playerInvetory.selectedItem.name != "")
                         {
+                            // checks if the amount of the selected item isn't 0
                             if (playerInvetory.selectedItem.amount != 0)
                             {
+                                // converts the world position to cell position.
                                 Vector3Int cellpos = tilemap.WorldToCell(new Vector3(cursor.transform.position.x, cursor.transform.position.y, 0));
+                                // check if the distance is with in the range limit
                                 if (Vector3.Distance(this.transform.position, cellpos) <= 3)
                                 {
+                                    // checks if the cell doesnt have a tile
                                     if (!tilemap.HasTile(cellpos))
                                     {
+                                        // checks if the cell position isn't the same as the player
                                         if (cellpos != this.transform.position && cellpos != new Vector3(this.transform.position.x, this.transform.position.y - 1, 0))
                                         {
+                                            // checks if there is a tile in the cells next to it.
                                             if (tilemap.HasTile(new Vector3Int(cellpos.x - 1, cellpos.y, 0)) || tilemap.HasTile(new Vector3Int(cellpos.x + 1, cellpos.y, 0)) || tilemap.HasTile(new Vector3Int(cellpos.x, cellpos.y - 1, 0)) || tilemap.HasTile(new Vector3Int(cellpos.x, cellpos.y + 1, 0)))
                                             {
+                                                // removes the item from your inventory
                                                 playerInvetory.Removeitem(playerInvetory.selectedItem.name, 1);
+                                                // sets the tile to the tile you are building with
                                                 tilemap.SetTile(cellpos, playerInvetory.selectedItem.tiles[Random.Range(0, playerInvetory.selectedItem.tiles.Length)]);
                                                 actiondelay = Time.time + 1;
                                             }
@@ -167,42 +199,58 @@ public class PlayerActions : MonoBehaviour
                     }
                 }
             }
+            // checks if pickaxe is equipped
             else if (pickaxeEquipped)
             {
+                // checks for input left mouse click
                 if (Input.GetKeyDown(KeyCode.Mouse0) && allowAction || Input.GetKeyDown(KeyCode.JoystickButton3) && allowAction)
                 {
-
-
+                    // converts the world position to cell position.
                     Vector3Int cellpos = tilemap.WorldToCell(new Vector3(cursor.transform.position.x, cursor.transform.position.y, 0));
 
+                    // check if the distance is with in the range limit
                     if (Vector3.Distance(this.transform.position, cellpos) <= 3)
                     {
+                        // checks if the cell has a tile
                         if (tilemap.HasTile(cellpos))
                         {
+                            // checks the tile name
                             switch (tilemap.GetTile(cellpos).name)
                             {
                                 case "Grass01":
+                                    // adds a item to your inventory
                                     playerInvetory.Additem("Grass", 1);
+                                    // removes the tile
                                     tilemap.SetTile(cellpos, null);
                                     break;
                                 case "Grass02":
+                                    // adds a item to your inventory
                                     playerInvetory.Additem("Grass", 1);
+                                    // removes the tile
                                     tilemap.SetTile(cellpos, null);
                                     break;
                                 case "Dirt01":
+                                    // adds a item to your inventory
                                     playerInvetory.Additem("Dirt", 1);
+                                    // removes the tile
                                     tilemap.SetTile(cellpos, null);
                                     break;
                                 case "Dirt02":
+                                    // adds a item to your inventory
                                     playerInvetory.Additem("Dirt", 1);
+                                    // removes the tile
                                     tilemap.SetTile(cellpos, null);
                                     break;
                                 case "Stone01":
+                                    // adds a item to your inventory
                                     playerInvetory.Additem("Stone", 1);
+                                    // removes the tile
                                     tilemap.SetTile(cellpos, null);
                                     break;
                                 case "DirtStone01":
+                                    // adds a item to your inventory
                                     playerInvetory.Additem("Stone", 1);
+                                    // removes the tile
                                     tilemap.SetTile(cellpos, null);
                                     break;
                                 case "Bedrock01":
@@ -216,30 +264,43 @@ public class PlayerActions : MonoBehaviour
                     }
                 }
             }
+            // checks if sword is equipped
             else if (swordEquipped)
             {
+                // checks for input pressed left mouse click
                 if (Input.GetKeyDown(KeyCode.Mouse0) && allowAction || Input.GetKeyDown(KeyCode.JoystickButton3) && allowAction)
                 {
+                    // sets a damage box collider on
                     swordBox.SetActive(true);
                 }
+                // checks for input release left mouse click
                 if (Input.GetKeyUp(KeyCode.Mouse0) && allowAction || Input.GetKeyDown(KeyCode.JoystickButton3) && allowAction)
                 {
+                    // sets a damage box collider off
                     swordBox.SetActive(false);
                 }
             }
         }
     }
+
+    // collision enter check
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // check if your overlapping a tree.
         if (collision.gameObject.CompareTag("Tree"))
         {
+            // sets overlapping tree
             overlappingTree = collision.gameObject;
         }
     }
+
+    // collision exit check
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // check if your overlapping a tree.
         if (collision.gameObject.CompareTag("Tree"))
         {
+            // sets overlapping tree
             overlappingTree = null;
         }
     }
--- Project 4/Assets/Scripts/Player/PlayerInvetory.cs
+++ Project 4/Assets/Scripts/Player/PlayerInvetory.cs
@@ -5,7 +5,9 @@ using UnityEngine.Tilemaps;
 
 public class PlayerInvetory : MonoBehaviour
 {
+    // [System.Serializable] Allows you to edit the struct in the inspector
     [System.Serializable]
+    // A struct that holds all the information that a item needs.
     public struct item {
         public item(string Name, int Amount, Sprite Image, bool Interactable, bool Food, int Foodvalue, bool BuildBlock, TileBase[] Tiles, bool IsSword, bool IsAxe, bool IsPickaxe)
         {
@@ -35,6 +37,7 @@ public class PlayerInvetory : MonoBehaviour
         public bool isPickaxe;
     }
 
+    // struct that holds all the information that a crafting item needs.
     [System.Serializable]
     public struct craftingItem
     {
@@ -66,9 +69,11 @@ public class PlayerInvetory : MonoBehaviour
     public GameObject selectedCraftingIcon;
     craftingItem selectedCraftingItem;
 
+    // the array of items in the game
     [Space]
     public item[] items;
 
+    // the array of crafting items in the game
     [Space]
     public craftingItem[] craftingItems;
 
@@ -77,33 +82,40 @@ public class PlayerInvetory : MonoBehaviour
 
     private void Start()
     {
+        // Gets the playerAction from the object this script is on.
         playerActions = GetComponent<PlayerActions>();
+        // makes a button for every crafting item.
         LoadCraftingItems();
     }
 
-    private void Update()
-    {
-    }
-
+    // Adds an item to your invetory
     public void Additem(string name, int amount)
     {
+        // Loop to loop through all the items that there are.
         for (int x = 0; x < items.Length; x++)
         {
+            // Checks if the item the loop is add has the same name as the one that should be added.
             if (items[x].name == name)
             {
+                // adds the amount you want to add to the item.
                 items[x].amount += amount;
+                // call a function that show a item has been added to your inventory
                 uIInfo.AddTextItem("Added " + amount + " " + name + " to your inventory.");
+                // checks if the item has a itemicon in your inventory yet.
                 if (!itemIcons.ContainsKey(name))
                 {
+                    // adds an icon to your inventory
                     GameObject justAdded = Instantiate(itemIcon, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), inventoryGrid.transform);
                     itemIcons.Add(name, justAdded);
                     uIInfo.CreateItemIcon(itemIcons[name], items[x].image, items[x].interactable);
                     uIInfo.UpdateItemIcon(itemIcons[name], items[x].amount.ToString());
                 }
+                // if there is it will update the icon to show the right amount
                 else
                 {
                     uIInfo.UpdateItemIcon(itemIcons[name], items[x].amount.ToString());
                 }
+                // if a item is selected it will update the item.
                 if (selectedItem.name == items[x].name)
                 {
                     selectedItem = items[x];
@@ -113,23 +125,32 @@ public class PlayerInvetory : MonoBehaviour
         }
     }
 
+    // removes an item from your inventory
     public void Removeitem(string name, int amount)
     {
+        // Loop to loop through all the items that there are.
         for (int x = 0; x < items.Length; x++)
         {
+            // Checks if the item the loop is add has the same name as the one that should be added.
             if (items[x].name == name)
             {
+                // removes the amount you want to add to the item.
                 items[x].amount -= amount;
+                // call a function that show a item has been removed from your inventory
                 uIInfo.AddTextItem("Removed " + amount + " " + name + " from your inventory.");
+                // checks if you dont have any of the item left in your inventory
                 if (items[x].amount <= 0)
                 {
+                    // destroy the icon and removes it from the list.
                     Destroy(itemIcons[name]);
                     itemIcons.Remove(name);
                 }
+                // checks if you still have the item in your inventory. and updates the amount the icon shows.
                 else
                 {
                     uIInfo.UpdateItemIcon(itemIcons[name], items[x].amount.ToString());
                 }
+                // if a item is selected it will update the item.
                 if (selectedItem.name == items[x].name)
                 {
                     selectedItem = items[x];
@@ -139,78 +160,101 @@ public class PlayerInvetory : MonoBehaviour
         }
     }
 
+    // Function that will be called when you click on a item that you can interact with
     public void InteractionInventory(GameObject itemIcon)
     {
+        // sets the itemname to none
         string itemname = "none";
         bool found = false;
+        // loops through all the item icons.
         foreach (var item in itemIcons)
         {
+            // checks if object are the same
             if(item.Value == itemIcon)
             {
+                // sets itemname to item.key name and set bool found to true
                 itemname = item.Key;
                 found = true;
                 break;
             }
         }
+        // checks if found is true
         if (found)
         {
+            // loops through all the items.
             for (int x = 0; x < items.Length; x++)
             {
+                // checks if item names are the same.
                 if (items[x].name == itemname)
                 {
-                    if (items[x].food)
+                    // checks if bool buildblock is true in the item.
+                    if (items[x].buildBlock)
                     {
-                        Removeitem(items[x].name,1);
-                    }
-                    else if (items[x].buildBlock)
-                    {
+                        // makes the item the selected item to build with.
                         selectedItem = items[x];
+                        break;
                     }
+                    // checks if bool isSword is true in the item.
                     else if (items[x].isSword)
                     {
+                        // if bool is true it will make all bool false
                         if (playerActions.swordEquipped)
                         {
                             playerActions.swordEquipped = false;
                             playerActions.pickaxeEquipped = false;
                             playerActions.axeEquipped = false;
+                            break;
                         }
+                        // if bool is false it will make the bool true and others false
                         else
                         {
                             playerActions.swordEquipped = true;
                             playerActions.pickaxeEquipped = false;
                             playerActions.axeEquipped = false;
+                            break;
                         }
                     }
+                    // checks if bool isPickaxe is true in the item.
                     else if (items[x].isPickaxe)
                     {
+                        // if bool is true it will make all bool false
                         if (playerActions.pickaxeEquipped)
                         {
                             playerActions.swordEquipped = false;
                             playerActions.pickaxeEquipped = false;
                             playerActions.axeEquipped = false;
+                            break;
                         }
+                        // if bool is false it will make the bool true and others false
                         else
                         {
                             playerActions.swordEquipped = false;
                             playerActions.pickaxeEquipped = true;
                             playerActions.axeEquipped = false;
+                            break;
                         }
                     }
+                    // checks if bool isAxe is true in the item.
                     else if (items[x].isAxe)
                     {
+                        // if bool is true it will make all bool false
                         if (playerActions.axeEquipped)
                         {
                             playerActions.swordEquipped = false;
                             playerActions.pickaxeEquipped = false;
                             playerActions.axeEquipped = false;
+                            break;
                         }
+                        // if bool is false it will make the bool true and others false
                         else
                         {
                             playerActions.swordEquipped = false;
                             playerActions.pickaxeEquipped = false;
                             playerActions.axeEquipped = true;
+                            break;
                         }
                     }
+                    // esle it will break the loop
                     else
                     {
                         break;
@@ -220,52 +264,69 @@ public class PlayerInvetory : MonoBehaviour
             found = false;
         }
     }
-
+    
+    // Function to load the crafting items
     public void LoadCraftingItems()
     {
+        // loops through all the crafting items.
         for (int x = 0; x < craftingItems.Length; x++)
         {
+            // makes an icon for the crafting item.
             GameObject justAdded = Instantiate(craftingItemIcon, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0), craftingInventoryGrid.transform);
             craftingItemIcons.Add(craftingItems[x].name, justAdded);
             uIInfo.CreateCraftingItemIcon(craftingItemIcons[craftingItems[x].name], craftingItems[x].image, true);
         }
     }
 
+    // Function that will be called when you click on a crafting item that you can interact with
     public void InteractionCrafting(GameObject itemIcon)
     {
+        // sets the itemname to none
         string itemname = "none";
         bool found = false;
+        // loops through all the crafting item icons.
         foreach (var item in craftingItemIcons)
         {
             if (item.Value == itemIcon)
             {
+                // sets itemname to item.key name and set bool found to true
                 itemname = item.Key;
                 found = true;
                 selectedCraftingIcon = itemIcon;
                 break;
             }
         }
+        // checks if found is true
         if (found)
         {
-            for (int x = 0; x < items.Length; x++)
+            // loops through all the crafting items.
+            for (int x = 0; x < craftingItems.Length; x++)
             {
+                // checks if the names are the same.
                 if (craftingItems[x].name == itemname)
                 {
+                    // set the crafting item to the selected crafting item.
                     selectedCraftingItem = craftingItems[x];
                     string text = "";
+                    // loops through all the items you need for the crafting item
                     for (int t = 0; t < craftingItems[x].needItemName.Length; t++)
                     {
                         int iGotAmount = 0;
+                        // loops through all the items.
                         for (int y = 0; y < items.Length; y++)
                         {
+                            // checks if the names are the same
                             if (items[y].name == craftingItems[x].needItemName[t])
                             {
+                                // set the amount you have
                                 iGotAmount = items[y].amount;
                                 break;
                             }
                         }
-                                text += craftingItems[x].needItemName[t] + ": " + iGotAmount + "/" + craftingItems[x].needItemAmount[t] + "\n";
+                        // adds text to the full text.
+                        text += craftingItems[x].needItemName[t] + ": " + iGotAmount + "/" + craftingItems[x].needItemAmount[t] + "\n";
                     }
+                    // sets the full text to a info text.
                     uIInfo.ShowCraftingInfo(craftingItems[x].image, text, craftingItems[x].name);
                     break;
                 }
@@ -274,13 +335,17 @@ public class PlayerInvetory : MonoBehaviour
         }
     }
 
+    // function for crafting
     public void Craft()
     {
         int enough = 0;
+        // loops through all the items you need for the crafting item
         for (int x = 0; x < selectedCraftingItem.needItemName.Length; x++)
         {
+            // loops through all items
             for (int y = 0; y < items.Length; y++)
             {
+                // checks if the names are the same and the amount is not 0
                 if (items[y].name == selectedCraftingItem.needItemName[x] && items[y].amount - selectedCraftingItem.needItemAmount[x] >= 0)
                 {
                     enough++;
@@ -288,13 +353,18 @@ public class PlayerInvetory : MonoBehaviour
                 }
             }
         }
+
+        // checks if you have enough items to craft the crafting item
         if (enough == selectedCraftingItem.needItemName.Length)
         {
+            // adds the items to your inventory
             Additem(selectedCraftingItem.name, selectedCraftingItem.amountGetting);
+            // removes the items you used
             for (int x = 0; x < selectedCraftingItem.needItemName.Length; x++)
             {
                 Removeitem(selectedCraftingItem.needItemName[x], selectedCraftingItem.needItemAmount[x]);
             }
+            // calls InteractionCrafting function to update displayed amount.
             InteractionCrafting(selectedCraftingIcon);
         }
     }
